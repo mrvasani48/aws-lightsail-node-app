e8157d1454d6f4007f477beabd2c18a875c002ca
mrvasani48 <vasanikishan48@gmail.com>
2025-09-24 14:45:31 +0530

    ip

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ const router = express.Router();
 router.get('/hello', (req, res) => {
     res.json({ message: 'Hello from the API route!' });
 });
+router.get('/ip', (req, res) => {
+    const clientIp = req.connection.remoteAddress;
+  console.log('Client IP:', clientIp);
+  res.json({ message: 'Hello from the API route!', ip: clientIp });
+});
 
 export default router;
